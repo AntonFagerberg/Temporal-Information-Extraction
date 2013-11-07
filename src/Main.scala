495257ada500a2934d6230e31d0d1e19720966d0
Anton Fagerberg <anton@antonfagerberg.com>
2013-11-07 15:10:18 +0100

    Added window of size 3.

--- src/Main.scala
+++ src/Main.scala
@@ -8,10 +8,18 @@ case class BaseSegmentation(
 )
 
 object Main {
+  val numberWords = List("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+
   val matches = List(
-    ("""(week)""".r, (week: String) => "P1W", "DURATION"),
-    ("""(day)""".r, (day: String) => "P1D", "DURATION"),
-    ("""([1-2]\d\d\d)""".r, (date: String) => date, "DATE")
+    (
+      ("^(" + numberWords.mkString("|") + ") years?$").r,
+      (value: String) => s"P${numberWords.indexWhere(numberWord => numberWord.r.findPrefixOf(value).isDefined)}Y",
+      "DURATION"
+    ),
+    ("^([1-2]\\d\\d\\d)-(\\d\\d)$".r, (value: String) => value, "DATE"),
+    ("^(week)$".r, (value: String) => "P1W", "DURATION"),
+    ("^(day)$".r, (value: String) => "P1D", "DURATION"),
+    ("^([1-2]\\d{3})$".r, (value: String) => value, "DATE")
   )
 
   def matchType(word: String): Option[(String, String)] = {
@@ -22,8 +30,9 @@ object Main {
     }
   }
 
-  def resultLine(segment: BaseSegmentation, category: String, value: String): String = {
-    s"${segment.source}\t${segment.lineNumber}\t${segment.wordNumber}\tt?\t$category\t$value"
+  def outputString(segment: BaseSegmentation, value: String, kind: String): String = {
+    s"""|${segment.source}\t${segment.lineNumber}\t${segment.wordNumber}\tt?\tvalue\t$value
+        |${segment.source}\t${segment.lineNumber}\t${segment.wordNumber}\tt?\tkind\t$kind""".stripMargin
   }
 
   def main(args: Array[String]): Unit = {
@@ -32,13 +41,34 @@ object Main {
       BaseSegmentation(lineSegments(0).trim, lineSegments(1).toInt, lineSegments(2).toInt, lineSegments(3).trim)
     }
 
-    for (segment <- lineSegments) {
-      matchType(segment.word) match {
-        case Some((value, kind)) => {
-          println(resultLine(segment, "value", value))
-          println(resultLine(segment, "type", kind))
+    for (segments <- lineSegments.sliding(3, 1)){
+      val segment1 = segments(0)
+      val segment2 = segments.lift(1)
+      val segment3 = segments.lift(2)
+
+      val result3 =
+        if (segment3.isEmpty) None
+        else matchType(s"${segment1.word} ${segment2.get.word} ${segment3.get.word}")
+
+      val result2 =
+        if (result3.isDefined || segment2.isEmpty) None
+        else matchType(s"${segment1.word} ${segment2.get.word}")
+
+      val result1 =
+        if (result3.isDefined || result2.isDefined) None
+          else matchType(segment1.word)
+
+
+      result3 map { case (value, kind) =>
+        println(outputString(segment1, value, kind))
+      } getOrElse {
+        result2 map { case (value, kind) =>
+          println(outputString(segment1, value, kind))
+        } getOrElse {
+          result1 foreach { case (value, kind) =>
+            println(outputString(segment1, value, kind))
+          }
         }
-        case _ =>
       }
     }
   }
