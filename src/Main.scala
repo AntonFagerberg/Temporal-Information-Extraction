65a82c1391cb3a1f5f83de20e28dec774f59a67c
Anton Fagerberg <anton@antonfagerberg.com>
2013-11-15 23:05:26 +0100

    Expanded window etc.

--- src/Main.scala
+++ src/Main.scala
@@ -1,4 +1,5 @@
 import java.io.{PrintWriter, File}
+import org.joda.time.LocalDate
 import scala.sys.process._
 
 case class BaseSegmentation(
@@ -14,6 +15,7 @@ case class Date(
   day: String
 ) {
   override def toString: String = s"$year-$month-$day"
+  def localTime = LocalDate.parse(this.toString)
 }
 
 object Main {
@@ -24,16 +26,56 @@ object Main {
     }}.toMap
 
   val numberWords = List("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
-  val monthWords = List("january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december")
+  val monthWords = List("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
 
   val matches =
     List(
+//      ( // [Perfect] January 1983
+//        monthWords.mkString("^(", "|", ")( ,)?( )?[1-2]\\d\\d\\d$").r,
+//        (value: String, segment: BaseSegmentation) =>
+//          s"${value.takeRight(4)}-${("0" + (1 + monthWords.indexWhere(_.r.findPrefixOf(value).isDefined))).takeRight(2)}",
+//        "DATE"
+//      ),
       (
-        "^now$".r,
-        (value: String, segment: BaseSegmentation) => "PRESENT_REF",
+        monthWords.mkString("^(", "|", ")( ,)?( )?\\d{1,2}( ,)?( )?[1-2]\\d\\d\\d$").r,
+        (value: String, segment: BaseSegmentation) =>
+          s"${value.takeRight(4)}-${("0" + (1 + monthWords.indexWhere(_.r.findPrefixOf(value).isDefined))).takeRight(2)}-${("0" + value.dropRight(4).filter(_.isDigit)).takeRight(2)}",
         "DATE"
       )
-    )
+//      (
+//        monthWords.mkString("^[Ll]ast (", "|", ")$").r,
+//        (value: String, segment: BaseSegmentation) =>
+//          s"${publicationDates(segment.source).localTime.minusYears(1).toString("YYYY")}-${("0" + (1 + monthWords.indexWhere(_.r.findPrefixOf(value.drop(5)).isDefined))).takeRight(2)}",
+//        "DATE"
+//      ),
+//      (
+//        monthWords.mkString("^(", "|", ") \\d{1,2}$").r,
+//        (value: String, segment: BaseSegmentation) =>
+//          s"${publicationDates(segment.source).localTime.minusYears(1).toString("YYYY")}-${("0" + (1 + monthWords.indexWhere(_.r.findPrefixOf(value.filter(!_.isDigit).trim).isDefined))).takeRight(2)}-${(0 + value.filter(_.isDigit).toInt).toString.takeRight(2)}",
+//        "DATE"
+//      ),
+//      (
+//        monthWords.mkString("^(", "|", ")$").r,
+//        (value: String, segment: BaseSegmentation) =>
+//          s"${publicationDates(segment.source).year}-${("0" + (1 + monthWords.indexWhere(_.r.findPrefixOf(value).isDefined))).takeRight(2)}",
+//        "DATE"
+//      )
+//      (
+//        "^((E|e)arly )?(T|t)his week$".r,
+//        (value: String, segment: BaseSegmentation) =>
+//          publicationDates(segment.source).localTime.toString("YYYY-'W'ww"),
+//        "DATE"
+//      )
+
+
+
+
+//      (
+//        "^a week$".r,
+//        (value: String, segment: BaseSegmentation) => "P1W",
+//        "DURATION"
+//      )
+//    )
 //      (
 //        "^\\d+ (day|month|year)s?$".r,
 //        (value: String, segment: BaseSegmentation) => {
@@ -107,34 +149,39 @@ object Main {
 //        (value: String, segment: BaseSegmentation) => value,
 //        "DATE"
 //      )
-//    )
+    )
 
   def matchType(word: String, segment: BaseSegmentation): Option[(String, String)] = {
-    val lowerCaseWord = word.toLowerCase
-
     matches find { case (regex, _, _) =>
-      regex.findFirstIn(lowerCaseWord).isDefined
+      regex.findFirstIn(word).isDefined
     } map { case (_, value, kind) =>
-      value(lowerCaseWord, segment) -> kind
+      value(word, segment) -> kind
     }
   }
 
   def outputTimesAttributes(segment: BaseSegmentation, value: String, kind: String): String = {
-    s"""|${segment.source}\t${segment.lineNumber}\t${segment.wordNumber}\tt-\tvalue\t$value
-        |${segment.source}\t${segment.lineNumber}\t${segment.wordNumber}\tt-\tkind\t$kind
+    s"""|${segment.source}\t${segment.lineNumber}\t${segment.wordNumber}\ttimex3\t1\tt-\tvalue\t$value
+        |${segment.source}\t${segment.lineNumber}\t${segment.wordNumber}\ttimex3\t1\tt-\ttype\t$kind
         |""".stripMargin
   }
 
-  def outputTimesExtents(segment: BaseSegmentation, lines: Int): String = {
-    {for (wordNumber <- segment.wordNumber until segment.wordNumber + lines) yield {
-        s"${segment.source}\t${segment.lineNumber}\t$wordNumber\tt-\t1\n"
-      }
-    }.mkString
-  }
+  val segmentBan =
+    List(
+      "^,$".r
+    )
 
-  def writeFiles(segment: BaseSegmentation, value: String, kind: String, lines: Int, timexAttributesWriter: PrintWriter, timexExtentsWriter: PrintWriter): Unit = {
-    timexAttributesWriter.append(outputTimesAttributes(segment, value, kind))
-    timexExtentsWriter.append(outputTimesExtents(segment, lines))
+  def outputTimesExtents(segments: List[BaseSegmentation]): String = {
+    for {
+      segment <- segments
+      if segmentBan.find(_.findFirstIn(segment.word.trim).isDefined).isEmpty
+    } yield {
+      s"${segment.source}\t${segment.lineNumber}\t${segment.wordNumber}\ttimex3\tt-\t1\n"
+    }
+  }.mkString
+
+  def writeFiles(segments: List[BaseSegmentation], value: String, kind: String, timexAttributesWriter: PrintWriter, timexExtentsWriter: PrintWriter): Unit = {
+    timexAttributesWriter.append(outputTimesAttributes(segments(0), value, kind))
+    timexExtentsWriter.append(outputTimesExtents(segments))
   }
 
   def main(args: Array[String]): Unit = {
@@ -159,31 +206,49 @@ object Main {
     val timexAttributesWriter = new PrintWriter(timexAttributesFile)
     val timexExtentsWriter = new PrintWriter(timexExtentsFile)
 
-    for (segments <- lineSegments.sliding(3, 1)) {
+    for (segments <- lineSegments.sliding(5, 1)) {
       val segment1 = segments(0)
       val segment2 = segments.lift(1)
       val segment3 = segments.lift(2)
+      val segment4 = segments.lift(3)
+      val segment5 = segments.lift(4)
+
+      val result5 =
+        if (segment5.isEmpty) None
+        else matchType(s"${segment1.word} ${segment2.get.word} ${segment3.get.word} ${segment4.get.word} ${segment5.get.word}", segment1)
+
+      val result4 =
+        if (result5.isDefined || segment4.isEmpty) None
+        else matchType(s"${segment1.word} ${segment2.get.word} ${segment3.get.word} ${segment4.get.word}", segment1)
 
       val result3 =
-        if (segment3.isEmpty) None
+        if (result5.isDefined || result4.isDefined || segment3.isEmpty) None
         else matchType(s"${segment1.word} ${segment2.get.word} ${segment3.get.word}", segment1)
 
       val result2 =
-        if (result3.isDefined || segment2.isEmpty) None
+        if (result5.isDefined || result4.isDefined || result3.isDefined || segment2.isEmpty) None
         else matchType(s"${segment1.word} ${segment2.get.word}", segment1)
 
       val result1 =
-        if (result3.isDefined || result2.isDefined) None
+        if (result5.isDefined || result4.isDefined || result3.isDefined || result2.isDefined) None
         else matchType(segment1.word, segment1)
 
-      result3 map { case (value, kind) =>
-        writeFiles(segment1, value, kind, 3, timexAttributesWriter, timexExtentsWriter)
+      result5 map { case (value, kind) =>
+        writeFiles(List(segment1, segment2.get, segment3.get, segment4.get, segment5.get), value, kind, timexAttributesWriter, timexExtentsWriter)
       } getOrElse {
-        result2 map { case (value, kind) =>
-          writeFiles(segment1, value, kind, 2, timexAttributesWriter, timexExtentsWriter)
+        result4 map { case (value, kind) =>
+          writeFiles(List(segment1, segment2.get, segment3.get, segment4.get), value, kind, timexAttributesWriter, timexExtentsWriter)
         } getOrElse {
-          result1 foreach { case (value, kind) =>
-            writeFiles(segment1, value, kind, 1, timexAttributesWriter, timexExtentsWriter)
+          result3 map { case (value, kind) =>
+            writeFiles(List(segment1, segment2.get, segment3.get), value, kind, timexAttributesWriter, timexExtentsWriter)
+          } getOrElse {
+            result2 map { case (value, kind) =>
+              writeFiles(List(segment1, segment2.get), value, kind, timexAttributesWriter, timexExtentsWriter)
+            } getOrElse {
+              result1 foreach { case (value, kind) =>
+                writeFiles(List(segment1), value, kind, timexAttributesWriter, timexExtentsWriter)
+              }
+            }
           }
         }
       }
@@ -194,5 +259,6 @@ object Main {
     timexExtentsWriter.close()
 
     println("/usr/bin/python tempeval2-data/scorer/score_entities.py tempeval2-data/training/english/data/base-segmentation.tab tempeval2-data/training/english/data/timex-extents.tab res/timex-extents.tab tempeval2-data/training/english/data/timex-attributes.tab res/timex-attributes.tab".!!)
+//    println("/usr/bin/python tempeval2-data/scorer/score_entities.py tempeval2-data/training/english/data/base-segmentation.tab tempeval2-data/training/english/data/timex-extents.tab tempeval2-data/training/english/data/timex-extents.tab tempeval2-data/training/english/data/timex-attributes.tab tempeval2-data/training/english/data/timex-attributes.tab".!!)
   }
 }
